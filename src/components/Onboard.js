b1c8ecbe258441230c4c1805a09e2af9c86a3047
William Tom <wtom002@uw.edu>
2024-05-09 15:43:15 -0700

    fix image pathing

--- src/components/Onboard.js
+++ src/components/Onboard.js
@@ -1,40 +1,40 @@
 import React from 'react';
 export default function Home() {
     return (
-        <><div class="home1">
+        <><div className="home1">
             <h1>how does it work</h1>
-            <div class="box-container">
-                <div class="box">
-                    <img src="../img/home/pink.png"></img>
+            <div className="box-container">
+                <div className="box">
+                    <img src="../img/home/pink.png" alt="pink"></img>
                     <h2>Upload a clear selfie of your face</h2>
                 </div>
 
-                <div class="box">
-                    <img src="purple.png"></img>
+                <div className="box">
+                    <img src="../img/home/purple.png" alt="purple"></img>
                     <h2>Answer skin survey</h2>
                 </div>
-                <div class="box">
-                    <img src="../img/home/green.png"></img>
+                <div className="box">
+                    <img src="../img/home/green.png" alt="green"></img>
                     <h2>Computer vision analyzes</h2>
                 </div>
-                <div class="box">
-                    <img src="../img/Home.png"></img>
+                <div className="box">
+                    <img src="../img/Home.png" alt="splash art"></img>
                     <h2>Get results with ingredients & products just for you</h2>
                 </div>
             </div>
-        </div><div class="grid-container">
-                <div class="grid-item">
+        </div><div className="grid-container">
+                <div className="grid-item">
                     <h1>computer vision</h1>
                     <p>(n.) a multidisciplinary field that enables computers to interpret and make sense of visual information from the world.</p>
                     <h2>The primary goal is to enable computers to extract meaningful insights, recognize patterns, and interpret visual data.</h2>
                 </div>
-                <div class="grid-item">
-                    <img src="image2.jpg" alt="Image 2"></img>
+                <div className="grid-item">
+                    <img src="image2.jpg" alt="pic 2"></img>
                 </div>
-                <div class="grid-item">
-                    <img src="image3.jpg" alt="Image 3"></img>
+                <div className="grid-item">
+                    <img src="image3.jpg" alt="pic 3"></img>
                 </div>
-                <div class="grid-item">
+                <div className="grid-item">
                     <h1>ingredient focus</h1>
                     <p>(n.) ingredients are substances added to cosmetic formulations to provide specific benefits to the skin.</p>
                     <h2>Understanding skincare ingredients is essential for making informed choices about your skincare routine.</h2>
