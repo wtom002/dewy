d0f788a6cab293b12225a1d4c29793b215d8abda
Erickuokuo <eric_kuo@outlook.com>
2024-05-09 15:15:08 -0700

    ingred

--- src/components/Ingredinary.js
+++ src/components/Ingredinary.js
@@ -3,9 +3,10 @@ import NavBar from './NavBar';
 
 export default function Ingredinary() {
   return (
-    <div class="banner banner-s bg-ingredinary">
+    <div class="dbanner banner-s bg-ingredinary">
       <NavBar darkMode={true} />
-        <h1>Ingredinary</h1>
+        <h1>skincare ingredient dictionary</h1>
+        <h2>your ultimate guide for accessing ingredient benefits</h2>
         </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import ResultsPage from './components/ResultsPage';
 import Home from './components/Home';
 import Scan from './components/Scan'
+import Ingredinary from './components/Ingredinary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home/>
+    <Ingredinary/>
   </React.StrictMode>
 );
 
