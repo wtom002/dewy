a0910f7d96eabfe134a880ad2110bb98cf0b329f
Erickuokuo <eric_kuo@outlook.com>
2024-05-17 06:32:55 -0700

    python files

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,17 @@ import NavBar from './NavBar';
 
 export default function About() {
   return (
-    <div className="banner banner-s bg-about">
+    <><div className="banner banner-s bg-about">
       <NavBar darkMode={false} />
-        <h1>nice to meet you,<br/>we are glazed donut</h1>
-        </div>
+      <h1>nice to meet you,<br />we are glazed donut</h1>
+    </div><div className="container-1">
+        <h1>how it started</h1>
+        <h2>How might we help individuals who are new to skincare gain immediate skincare knowledge and enable them to swiftly address their skin concerns by suggesting the most suitable ingredients in skincare products?</h2>
+      </div>
+      <div className="container-2">
+        <div className="leftcol"></div>
+      </div>
+
+      </>
   );
 }
