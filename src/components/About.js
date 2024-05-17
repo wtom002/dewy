2372a74e4e4e394dc2a768b023e8a8c81b9a97f2
Erickuokuo <eric_kuo@outlook.com>
2024-05-17 07:23:30 -0700

    add images

--- src/components/About.js
+++ src/components/About.js
@@ -11,9 +11,25 @@ export default function About() {
         <h2>How might we help individuals who are new to skincare gain immediate skincare knowledge and enable them to swiftly address their skin concerns by suggesting the most suitable ingredients in skincare products?</h2>
       </div>
       <div className="container-2">
-        <div className="leftcol"></div>
+        <div className="leftcol">
+        <h1>our inspiration</h1>
+        <div className="inspo-grid">
+          <div className="inspo" style={{ backgroundImage: "url('components/images/overload.png')" }}>
+            <p>Social Media Misinformation</p>
+          </div>
+          <div className="inspo">
+            <p>Trend-Driven Purchases</p>
+          </div>
+          <div className="inspo">
+            <p>Harmful Skincare Ingredients</p>
+          </div>
+          <div className="inspo">
+            <p>Information Overload</p>
+          </div>
+        </div>
+        </div>
+        <h2>Our goal is to help skincare beginners navigate the complexities of skincare and support them in making informed decisions.</h2>
       </div>
-
       </>
   );
 }
