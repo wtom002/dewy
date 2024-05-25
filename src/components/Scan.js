285f3a685ab0be7c87e3a4cb62406a22dd707520
William Tom <97192579+wtom002@users.noreply.github.com>
2024-05-24 21:03:02 -0700

    Update Scan.js

--- src/components/Scan.js
+++ src/components/Scan.js
@@ -39,7 +39,7 @@ export default function ImageUpload() {
     const formData = new FormData();
     formData.append('file', selectedFile);
     try {
-      const response = await axios.post('http://127.0.0.1:5000/upload', formData, {
+      const response = await axios.post('dewy-lco6.vercel.app/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
