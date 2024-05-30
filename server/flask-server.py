7f5672b97fa99a04d4846e9c4c4257abf110e0da
William Tom <97192579+wtom002@users.noreply.github.com>
2024-05-29 18:40:04 -0700

    edit

--- server/flask-server.py
+++ server/flask-server.py
@@ -7,7 +7,7 @@ import io
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "https://dewy-glazed-donuts.vercel.app"}})
+CORS(app, resources={r"/*": {"origins": "https://dewy-glazed-donuts.vercel.app/"}})
 # app.config['CORS_HEADERS'] = 'Content-Type'
 @app.after_request
 def add_cors_headers(response):
@@ -33,7 +33,7 @@ def preprocess_image(image):
     image = np.expand_dims(image, axis=0)  
     return image
 
-@app.route('/api/upload', methods=['OPTIONS'])
+@app.route('/upload', methods=['OPTIONS'])
 def upload_options():
     response = jsonify({})
     response.headers['Access-Control-Allow-Origin'] = 'https://dewy-glazed-donuts.vercel.app/'
@@ -41,7 +41,7 @@ def upload_options():
     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE'
     return response
     
-@app.route('/api/upload', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_image():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
